4f4ada3af3491dc6cf9c42ea2f29c95ace81d071
AkihikoNambu <mate0rich.nmb@gmail.com>
2015-06-06 17:55:35 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'notes/new' => 'notes#new'
+  get '/notes/new' => 'notes#new'
   # notesコントローラのcreateアクションへのルーティングを設定してください
   
 
   root 'home#top'
-  get 'about' => 'home#about'
+  get '/about' => 'home#about'
   
 end
