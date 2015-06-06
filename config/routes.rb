e99ce56289f80fe39d943b231399582313b7ab34
AkihikoNambu <mate0rich.nmb@gmail.com>
2015-06-06 17:56:59 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'notes/new'
-  post 'notes' => 'notes#create'
-  get 'notes' => 'notes#index'
+  get '/notes/new'
+  post '/notes' => 'notes#create'
+  get '/notes' => 'notes#index'
   # showアクションへのルーティングを設定してください
 
   root 'home#top'
-  get 'about' => 'home#about'
+  get '/about' => 'home#about'
   
 end
