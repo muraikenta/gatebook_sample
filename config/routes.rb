5eb0331d49d1ccb94055ec7eb6916aeb9171860d
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-06-06 16:20:31 +0900

    page21-routes.rb-fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'notes/new'
-  post 'notes' => 'notes#create'
-  get 'notes' => 'notes#index'
-  get 'notes/:id' => 'notes#show', as: 'note'
-  get 'notes/:id/edit' => 'notes#edit', as: 'edit_note'
-  patch 'notes/:id' => 'notes#update', as: 'update_note'
+  get '/notes/new' => 'notes#new'
+  post '/notes' => 'notes#create'
+  get '/notes' => 'notes#index'
+  get '/notes/:id' => 'notes#show', as: 'note'
+  get '/notes/:id/edit' => 'notes#edit', as: 'edit_note'
+  patch '/notes/:id' => 'notes#update', as: 'update_note'
   # destroyアクションへのルーティングを設定してください
   
 
   root 'home#top'
-  get 'about' => 'home#about'
+  get '/about' => 'home#about'
   
 end
