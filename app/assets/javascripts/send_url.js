50ebe8eb039c393be531f4bb869f1d65ebfcd084
Kenta Murai <muraikenta@users.noreply.github.com>
2015-06-15 17:35:47 +0900

    Update send_url.js

--- app/assets/javascripts/send_url.js
+++ app/assets/javascripts/send_url.js
@@ -1,3 +1,8 @@
 $(document).on('page:load', function() {
-  window.parent.postMessage({ location: window.location.pathname }, "*");
+  window.parent.postMessage({ location: window.location.pathname, width: body_width }, "*");
+});
+
+$(function(){
+  var body_width = $('body')[0].scrollWidth;
+  window.parent.postMessage({ width: body_width }, "*");
 });
