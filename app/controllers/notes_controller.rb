74574558d320c7f466c097f6d0a421829ed66bf6
Masa <cmasad743@gmail.com>
2015-06-24 13:38:39 +0900

    page3 done

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,12 @@
 class NotesController < ApplicationController
+  # before_actionを設定してください
+
   def index
     @notes = Note.all
   end
 
   def show
+    # 以下の一行を削除してください
     @note = Note.find(params[:id])
   end
 
@@ -19,10 +22,12 @@ class NotesController < ApplicationController
   end
 
   def edit
+    # 以下の一行を削除してください
     @note = Note.find(params[:id])
   end
 
   def update
+    # 以下の一行を削除してください
     @note = Note.find(params[:id])
     @note.title = params[:title]
     @note.content = params[:content]
@@ -31,8 +36,12 @@ class NotesController < ApplicationController
   end
 
   def destroy
+    # 以下の一行を削除してください
     @note = Note.find(params[:id])
     @note.destroy
     redirect_to notes_path
   end
+
+  # set_noteメソッドをprivateメソッドとして定義してださい
+
 end
