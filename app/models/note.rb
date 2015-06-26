4727f3655755b40471b18914c2da2cc8ad6f7e53
Masa <cmasad743@gmail.com>
2015-06-26 11:17:19 +0900

    page19 done

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ActiveRecord::Base
+  # belongs_toを用いて「それぞれのnoteはuserに属している」という関係性を設定してください
+  
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  # has_manyを用いて「ひとりのuserは複数のnoteを持つ」という関係性を設定してください
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
