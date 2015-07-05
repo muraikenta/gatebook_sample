b99d16dc45dd7f414435bbd3ba3fe9af343e0204
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-07-05 23:08:24 +0900

    advanced6-comp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
-  # beforeアクションを定義してください
-
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include ApplicationHelper
 
-  # privateメソッドを定義し、deviseの新規登録フォームでnameを受け取れるようにしてください
 
-  
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end 
 end
