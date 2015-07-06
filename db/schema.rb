28300abc4f787a2e04f9aca65e5b781a5f9118a5
Masa <cmasad743@gmail.com>
2015-07-06 17:28:00 +0900

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626001135) do
+ActiveRecord::Schema.define(version: 20150628131539) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150626001135) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
