c548eac1aebf4b09f31b62760262909150f3d8f2
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-07-06 20:21:18 +0900

    advanced10-comp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   def top
-    # if~else文を貼り付けてください
-    
-    
+    if user_signed_in?
+      @note = Note.new
+      @notes = Note.all.order(created_at: :desc)
+    else
+      @message = "ようこそGatebookへ！"
+    end
   end
   
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   # onlyを用いて、生成されるルーティングを指定してください
-  resources :notes
+  resources :notes, only: [:show, :create, :edit, :update, :destroy]
 
   root 'home#top'
   get '/about' => 'home#about'
