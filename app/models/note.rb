1b471207981ff1ea94470d369f7184cea72935b9
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-07-07 16:24:03 +0900

    advanced23-curriculum

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,10 @@
 class Note < ActiveRecord::Base
   belongs_to :user
+
+  # Noteのデータが削除されたときに、それに紐付いたいいね！も消えるようにしてください
   has_many :likes
+  
+
   has_many :liking_users, through: :likes, source: :user
 
   validates :title, presence: true
