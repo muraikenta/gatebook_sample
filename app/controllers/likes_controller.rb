7c8a40ffa2c8e4e7db37f680f4cb235f16f750fa
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-07-07 20:33:15 +0900

    advanced25-curriculum-fix

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   def like
+    # 変数noteを変数@noteに変更してください
     note = Note.find(params[:note_id])
     like = current_user.likes.build(note_id: note.id)
     like.save
