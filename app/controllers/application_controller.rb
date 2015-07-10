8d98489161292d367e19a54a7bfa6de4e6b1b46b
AkihikoNambu <mate0rich.nmb@gmail.com>
2015-07-10 14:56:44 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   # beforeアクションを定義してください
-
-
+  
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   # privateメソッドを定義し、deviseの新規登録フォームでnameを受け取れるようにしてください
-
+  
   
 end
