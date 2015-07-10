64f7a70da6715575cfee1852daeb984589368642
AkihikoNambu <mate0rich.nmb@gmail.com>
2015-07-10 15:01:24 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   # beforeアクションでcorrect_userメソッドを呼び出してください
-
-
+  
+  
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -38,6 +38,6 @@ class UsersController < ApplicationController
     end
 
     # correct_userメソッドを定義してください。
-
-
+    
+    
 end
