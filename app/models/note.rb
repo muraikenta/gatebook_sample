7819e99ecddd6c2998259784f80bae0479b29fbe
AkihikoNambu <mate0rich.nmb@gmail.com>
2015-07-10 15:13:47 +0900

    Update note.rb

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,8 @@
 class Note < ActiveRecord::Base
   belongs_to :user
   # likesテーブルとの関係性を定義してください
-
+  
+  
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
