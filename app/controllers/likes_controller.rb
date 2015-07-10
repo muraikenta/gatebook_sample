b17a1a85ae584ba4f0b8b3ced9e384ceb5101447
AkihikoNambu <mate0rich.nmb@gmail.com>
2015-07-10 15:18:48 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,17 @@
 class LikesController < ApplicationController
   def like
     # 変数noteに、findメソッドを用いてNoteインスタンスを取得し、代入してください
-
-
+    
+    
     # 変数likeに、current_userとbuildを用いてLikeインスタンスを代入してください
-
-
+    
+    
     # saveメソッドで、likeを保存してください
-
-
+    
+    
     # 最後に、notesコントローラののshowアクションにリダイレクトしてください
     
-
+    
   end
 
   def dislike
