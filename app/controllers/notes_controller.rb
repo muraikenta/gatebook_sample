e185b9876f6258d693d882acbd30e38b0ab24208
Kenta Murai <muraikenta@users.noreply.github.com>
2015-08-09 20:40:48 +0900

    Update notes_controller.rb

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,11 @@
 class NotesController < ApplicationController
   def new
   end
-  
-  # createアクションを作成してください
-  
-  
+
+  def create
+    # フォームのname属性がtitleの値を、@titleに代入してください
+    
+    # フォームのname属性がcontentの値を、@contentに代入してください
+    
+  end
 end
